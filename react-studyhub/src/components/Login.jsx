b65cbeb4cec058265377d3479aabd2d2ff6ed3ec
yamadaMk12 <kite.kun555@gmail.com>
2025-12-23 13:03:37 +0100

    feat: update Login component to use refs for input fields and enhance styling for login form

--- react-studyhub/src/components/Login.jsx
+++ react-studyhub/src/components/Login.jsx
@@ -1,19 +1,26 @@
-import { useState, useRef } from "react";
+import { useRef } from "react";
 
 export default function Login({ onLogin, error }) {
-  const [password, setPassword] = useState("");
-  const inputRef = useRef(null);
+  const passwordRef = useRef(null);
+  const userRef = useRef(null);
 
+  console.log("render login")
   return (
-    <div>
+    <div className="login">
       <h2>Connexion</h2>
+      <label htmlFor="username">Username</label>
       <input
-        ref={inputRef}
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        id="username"
+        type="text"
+        ref={userRef}
       />
-      <button onClick={() => onLogin(password)}>Login</button>
+      <label htmlFor="pass">Password</label>
+      <input
+        id="pass"
+        ref={passwordRef}
+        type="password"
+      />
+      <button onClick={() => onLogin(passwordRef.current)}>Login</button>
       {error && <p style={{ color: "red" }}>{error}</p>}
     </div>
   );
